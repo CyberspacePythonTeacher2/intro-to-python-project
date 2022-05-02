36cd323436919d5a64bca8585e9a317d61b3eb52
CyberspacePythonTeacher2 <team2.cyberspace.python@gmail.com>
2022-05-02 22:43:25 +0200

    Add week 2 python code

---
+++ week_2.py
@@ -0,0 +1,16 @@
+# variable declaration
+player_choice = "rock"
+computer_choice = "paper" # Good ol rock, nothing beats that
+is_player_win = False
+is_computer_win= True
+
+# display message
+print("Welcome to the game of Rock Paper Scissors.")
+
+# display choices
+print("Player's choice: " + player_choice)
+print("Computer's choice: " + computer_choice)
+
+# display who wins
+print("Is player wins:",is_player_win)
+print("Is computer wins: ",is_computer_win)
