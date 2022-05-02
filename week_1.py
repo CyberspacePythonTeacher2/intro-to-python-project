1e0323a43a89e21e99e518aa1f27b2bcf42a356a
CyberspacePythonTeacher2 <team2.cyberspace.python@gmail.com>
2022-05-02 22:32:49 +0200

    Add week 1 python code

---
+++ week_1.py
@@ -0,0 +1,5 @@
+print("Welcome to the game of Rock Paper Scissors.")
+print("The winning rules for the game are as follows:\n"
+                                +"   "+"Rock vs Paper    -> Paper wins\n"
+                                +"   "+"Rock vs Scissors  -> Rock wins\n"
+                                +"   "+"Paper vs Scissors -> Scissors wins\n")
